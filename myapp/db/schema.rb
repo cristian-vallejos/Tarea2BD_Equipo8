34d6e54f9c987c0c020e74b19e323291ed1da6aa
Pedro Mérida <pmerida99@gmail.com>
2020-01-15 22:53:53 -0300

    Se crearon 2 tablas en la base de datos, una de productos y otra de inventario. Tienen pocos elementos ambas, falta agregarle mas

---
+++ myapp/db/schema.rb
@@ -0,0 +1,43 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_01_16_002730) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "inventarios", force: :cascade do |t|
+    t.text "producto"
+    t.integer "stock"
+    t.integer "productoid"
+    t.integer "proveedorid"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "titulo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "productos", force: :cascade do |t|
+    t.integer "productoid"
+    t.text "animal"
+    t.text "tipo"
+    t.text "descripcion"
+    t.integer "precio"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
